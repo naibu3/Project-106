aa1e8b54ae250eed4282e3fd9e41dece5cded12c
naibu3 <i12cacgp@uco.es>
2022-12-01 12:29:17 +0100

    app_main added

--- src/gestor_cursos/gestor_cursos.cc
+++
@@ -1,7 +0,0 @@
-/*
-gestor_cursos.h
-
-Clase que implementara la UI y la integracion del resto de clases.
-*/
-
-#include "gestor_cursos.h"
--- src/gestor_cursos/gestor_cursos_main.cc
+++
@@ -1,7 +0,0 @@
-#include "gestor_cursos.h"
-
-int main(int argc, char const *argv[])
-{
-
-    return 0;
-}

b1baf9f3f5c5156ab81454bccc213bc6c538f093
pabcasm <pcmoncalvillo@gmail.com>
2022-12-07 13:17:13 +0100

    arreglados errores 2

--- src/usuario/usuario.cc
+++ src/usuario/usuario.cc
@@ -4,6 +4,6 @@ std::string Usuario::set_id(){
     srand(time(NULL));
     long identificador;
     identificador=1+rand()%(100000001-1);
-    std::string id=std::string to_string(long identificador);
+    std::string id = std::to_string(identificador);
     return id;
 }

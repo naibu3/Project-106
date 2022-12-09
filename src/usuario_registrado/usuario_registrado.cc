ab281a2ddf5918dbbad9e482a466f68bfec6cf5e
agaravi <118843315+agaravi@users.noreply.github.com>
2022-12-09 18:08:47 +0100

    Update usuario_registrado.cc

--- src/usuario_registrado/usuario_registrado.cc
+++ src/usuario_registrado/usuario_registrado.cc
@@ -1,19 +1,17 @@
 /*
 usuario.cc
 
-Clase que representa al usuario no registrado.
+Clase que representa al usuario registrado.
 */
 
 #include "usuario_registrado.h"
 
-bool Usuario_registrado::inscribirse(Curso c,std::list<Curso> lista_cursos_){
+void Usuario_registrado::inscribirse(Curso c,std::list<Curso> lista_cursos_){ //Función que se ejecuta si todos los requisitos de la funcion inscribir_usuario de Curso se cumplen.
     std::string id_curso=c.get_id();
     std::list<Curso>::iterator it;
     for(it=lista_cursos_.begin();it!=lista_cursos_.end();it++){
         if(it->get_id()==id_curso){
             cursos_inscritos_.push_back(c);
-            return true;
-        }
+            printf("Inscripción completada correctamente");
     }
-   return false;
 }

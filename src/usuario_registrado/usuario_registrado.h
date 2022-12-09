1cfb55ed858136c6db27e252de163f5b559ce832
agaravi <118843315+agaravi@users.noreply.github.com>
2022-12-09 18:01:37 +0100

    Update usuario_registrado.h

--- src/usuario_registrado/usuario_registrado.h
+++ src/usuario_registrado/usuario_registrado.h
@@ -16,16 +16,19 @@ Clase que representa al usuario no registrado.
 class Usuario_registrado : public Usuario {
 
     private:
-        std::string username_, email_;
+        std::string username_, email_,estudios_;
         std::list<Curso> cursos_inscritos_;
 
 
     public:
         Usuario_registrado(std::string username,
-            std::string email);
+            std::string email, std::string estudios);
 
         inline std::string get_username(){return username_;}
         inline std::string get_email(){return email_;}
+        inline std::string get_estudios(){return estudios_;}
+    //std::string set_estudios();
+    
         bool inscribirse(Curso c, std::list<Curso> lista_cursos_);
 
 };

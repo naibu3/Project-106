7eb2624d6aae0cfb982f62bcdd6ca1584fc7fb3b
naibu3 <i12cacgp@uco.es>
2022-12-13 17:28:13 +0100

    Main añadido

--- build/src/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/naibu3/ingInformatica/IS/Project-106/src/app
-# Build directory: /home/naibu3/ingInformatica/IS/Project-106/build/src/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

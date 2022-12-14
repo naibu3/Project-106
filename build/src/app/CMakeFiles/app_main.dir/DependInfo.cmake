2678caeb1bcd2f68b7ed86f77040e8a54cc81d5a
naibu3 <i12cacgp@uco.es>
2022-12-14 17:38:32 +0100

    Ver lista añadido al menu

---
+++ build/src/app/CMakeFiles/app_main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/naibu3/ingInformatica/IS/Project-106/src/app/app_main.cc" "/home/naibu3/ingInformatica/IS/Project-106/build/src/app/CMakeFiles/app_main.dir/app_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/app"
+  "../src/lista_cursos"
+  "../src/curso"
+  "../src/usuario_registrado"
+  "../src/usuario"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/naibu3/ingInformatica/IS/Project-106/build/src/lista_cursos/CMakeFiles/lista_cursos.dir/DependInfo.cmake"
+  "/home/naibu3/ingInformatica/IS/Project-106/build/src/curso/CMakeFiles/curso.dir/DependInfo.cmake"
+  "/home/naibu3/ingInformatica/IS/Project-106/build/src/usuario_registrado/CMakeFiles/usuario_registrado.dir/DependInfo.cmake"
+  "/home/naibu3/ingInformatica/IS/Project-106/build/src/usuario/CMakeFiles/usuario.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/lista_cursos/CMakeFiles/lista_cursos.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "liblista_cursos.a"
+)

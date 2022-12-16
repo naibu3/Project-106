9c7264c6b5f118a1961e512dad2196deb61528d2
naibu3 <i12cacgp@uco.es>
2022-12-16 11:57:25 +0100

    Instalador v0.2

--- linux_install.sh
+++ linux_install.sh
@@ -1,11 +1,22 @@
 !# /bin/bash
 
+verde='\033[32m'
+rosa='\033[35m'
+reset='\033[0m'
+
+echo "\n${rosa}Building...${reset}\n"
 cd build;
-
 cmake ..;
-
 make;
+echo "\n${verde}Built! (We hope)${reset}"
 
-cp ./../src/app/banner.txt ./..
-cp ./../src/lista_cursos/lista_cursos.txt ./..
-cp ./src/app/app_main ./../gestor_cursos
+cd ..;
+echo "${rosa}Creating symlink to files...${reset}\n"
+echo ''
+rm banner.txt lista_cursos.txt;
+ln -s ./src/app/banner.txt;
+ln -s ./src/lista_cursos/lista_cursos.txt;
+echo "\n${rosa}Adding executable to current folder...${reset}\n"
+cp ./build/src/app/app_main ./gestor_cursos;
+
+echo "\n${verde}[ OK ] Done${reset}\n"

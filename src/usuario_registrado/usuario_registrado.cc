330001d40f97253ee067a97d4b6e388848c26b8f
agaravi <118843315+agaravi@users.noreply.github.com>
2022-12-18 13:18:56 +0100

    Función de hora actualizada

--- src/usuario_registrado/usuario_registrado.cc
+++ src/usuario_registrado/usuario_registrado.cc
@@ -25,38 +25,30 @@ bool Usuario_registrado::inscribirse(Curso c){
     }
 
     //COMPROBAR SI EL CURSO YA HA COMENZADO
-    /*
     auto now = chrono::system_clock::now();
     time_t end_time = chrono::system_clock::to_time_t(now);
     std::string ahora = ctime(&end_time); //fecha actual
-    std::string ano_actual = ahora.substr(20,23); //comprobación de año
+    std::string ano_actual = ahora.substr(ahora.length()-5,4); //comprobación de año
     int actual_year = stoi(ano_actual);
     if(actual_year>year){
         printf("El curso ya ha comenzado\n");
         return false;
     }
-    std::string mes_actual = ahora.substr(4,6); //comprobación de mes
+    std::string mes_actual = ahora.substr(4,3); //comprobación de mes
     std::string diccionario_meses[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
     int nmes_actual, nmes_curso;
     for(int i = 0; i < 12; i++){ //pasa los meses a numeros
         if(mes_actual == diccionario_meses[i]){
-            nmes_actual = i;
+            nmes_actual = i+1;
         }
         else if(month == diccionario_meses[i]){
-            nmes_curso = i;
+            nmes_curso = i+1;
         }
     }
     if(nmes_actual>nmes_curso){
         printf("El curso ya ha comenzado\n");
         return false;
     }
-    std::string dia_actual = ahora.substr(9,9); //comprobación de día
-    int actual_day = stoi(dia_actual);
-    if(actual_day>day){
-        printf("El curso ya ha comenzado\n");
-        return false;
-    }
-    */
 
     //COMPROBAR QUE NO ESTUVIERAS YA INSCRITO
     if(!c.inscribir_usuario(this->get_id())){
